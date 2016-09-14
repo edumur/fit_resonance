e357ec0605c8cb80a85b1b3b102e67b07f4b6152
edumur <etienne.dumur@gmail.com>
2016-09-14 14:22:29 -0500

    Works with 2D file now.

--- labrad.py
+++ labrad.py
@@ -47,6 +47,8 @@ class labrad(tools):
         self._folder = folder
         self._file_number = file_number
 
+        # _folder and _file_number have to be defined before _get_full_name is
+        # called
         self._full_name = self._get_full_name()
 
         self._frequency_unit = None
@@ -75,6 +77,7 @@ class labrad(tools):
 
         if len(f) < 1:
             raise ValueError('Your file_number is incorrect, no file found.')
+
         return f[0]
 
 
@@ -99,11 +102,29 @@ class labrad(tools):
 
         Return
         ----------
-        nb_point : float
+        nb_point : int
             Number of data points
+            If the number of dimensions in greater than one, return a tuple
         """
 
-        return len(self._data[0])
+        if self._dimension == 2:
+            return len(np.unique(self._data[0])), len(np.unique(self._data[1]))
+        elif self._dimension == 1:
+            return len(self._data[0])
+
+
+
+    def get_number_dimension(self):
+        """
+        Return the number of sweeped parameters
+
+        Return
+        ----------
+        nb_dimension : int
+            Number of dimension
+        """
+
+        return self._dimension
 
 
 
@@ -113,27 +134,29 @@ class labrad(tools):
         """
 
         s = ConfigParser()
-        print self._full_name[:-3]+'ini'
+
         s.read(os.path.join(os.getcwd(), self._full_name[:-3]+'ini'))
-        print s.sections()
 
-        # If the file is 1d
-        self._frequency_unit = s.get('Independent 1', 'units').lower()
+        # Contain information on the sweeped parameters
+        self._sweeped_labels = []
+        self._sweeped_units = []
 
+        # Contain information about the measured parameters
         self._parameters = {'s11', 's12', 's21', 's22'}
         self._units = []
         self._measured = []
 
         # Attribut which will contain the number dimension of the measurement
-        self.dimension = 0
+        self._dimension = 0
 
         # We browse all sections and options of the file
         for section in s.sections():
             # We select only the Independent sections
             if section.split()[0] == 'Independent':
-                # if s.get(section, 'label')
+                self._sweeped_labels.append(s.get(section, 'label').lower())
+                self._sweeped_units.append(s.get(section, 'units').lower())
 
-                self.dimension += 1
+                self._dimension += 1
 
             # We select only the Dependent sections
             if section.split()[0] == 'Dependent':
@@ -144,6 +167,9 @@ class labrad(tools):
                         self._units.append(s.get(section, option).lower())
                     elif option == 'category':
                         self._measured.append(s.get(section, option).lower())
+
+
+        self._frequency_unit = self._sweeped_units[self._sweeped_labels.index('frequency')]
 
         self._unit_mag = self._units[0]
         self._unit_phase = self._units[-1]
@@ -187,64 +213,77 @@ class labrad(tools):
         if data_format.lower() not in ('ma', 'db', 'ri'):
             raise ValueError('The argument "data_format" should be in the form : "ma", "ri", "db"')
 
+
         #For concision we create short variable names
         d = self._data
         f = self.frequency_factor(self._frequency_unit)
         m = self._measured.index(s) + 1 # Return position of the magnitude data
 
-        # Depending on the file format and the asked format we return data
+        # Depending on the file format and the asked format we get data
         if self._unit_mag == 'ma' and data_format.lower() == 'db':
 
-            return (d[0]*f,
-                    self.ma2db(d[m]),
-                    d[m+4])
+            a = self.ma2db(d[m]),
+            b = d[m+4]
 
         elif self._unit_mag == 'db' and data_format.lower() == 'ma':
 
-            return (d[0]*f,
-                    self.db2ma(d[m]),
-                    d[m+4])
+            a = self.db2ma(d[m]),
+            b = d[m+4]
 
         elif self._unit_mag == 'db' and data_format.lower() == 'ri':
 
             if self._unit_phase == 'rad':
 
-                return (d[0]*f,
-                        np.cos(d[m+4])*self.db2ma(d[m]),
-                        np.sin(d[m+4])*self.db2ma(d[m]))
+                a = np.cos(d[m+4])*self.db2ma(d[m]),
+                b = np.sin(d[m+4])*self.db2ma(d[m])
             else:
 
-                return (d[0]*f,
-                        np.cos(np.radians(d[m+4]))*self.db2ma(d[m]),
-                        np.sin(np.radians(d[m+4]))*self.db2ma(d[m]))
+                a = np.cos(np.radians(d[m+4]))*self.db2ma(d[m]),
+                b = np.sin(np.radians(d[m+4]))*self.db2ma(d[m])
 
         elif self._unit_mag == 'ma' and data_format.lower() == 'ri':
 
             if self._unit_phase == 'rad':
 
-                return (d[0]*f,
-                        np.cos(d[m+4])*d[m],
-                        np.sin(d[m+4])*d[m])
+                a = np.cos(d[m+4])*d[m],
+                b = np.sin(d[m+4])*d[m]
 
             else:
 
-                return (d[0]*f,
-                        np.cos(np.radians(d[m+4]))*d[m],
-                        np.sin(np.radians(d[m+4]))*d[m])
+                a = np.cos(np.radians(d[m+4]))*d[m],
+                b = np.sin(np.radians(d[m+4]))*d[m]
 
         elif self._unit_mag == 'ri' and data_format.lower() == 'ma':
 
-            return (d[0]*f,
-                    np.sqrt(d[m]**2 + d[m+4]**2),
-                    np.degrees(np.angle(d[m] + 1j*d[m+4])))
+            a = np.sqrt(d[m]**2 + d[m+4]**2),
+            b = np.degrees(np.angle(d[m] + 1j*d[m+4]))
 
         elif self._unit_mag == 'ri' and data_format.lower() == 'db':
 
-            return (d[0]*f,
-                    self.ma2db(np.sqrt(d[m]**2 + d[m+4]**2)),
-                    np.degrees(np.angle(d[m] + 1j*d[m+4])))
+            a = self.ma2db(np.sqrt(d[m]**2 + d[m+4]**2)),
+            b = np.degrees(np.angle(d[m] + 1j*d[m+4]))
 
         else:
-            return (d[0]*f,
-                    d[m],
-                    d[m+4])
+            a = d[m],
+            b = d[m+4]
+
+        # Depending of the number of dimensions we return data
+        if self._dimension == 2:
+
+            # Find index for sweeped parameters
+            l  = self._sweeped_labels.index('frequency')
+            temp = np.array([0, 1])
+            ll = temp[temp!=l][0]
+
+            # Return 1D array for sweeped parameters and 2D array for measured
+            # one.
+            n = len(np.unique(d[l]))
+            p = d[ll][::n]
+            f = d[l][:n]*f
+            a = np.reshape(a, (len(p), len(f)))
+            b = np.reshape(b,  (len(p), len(f)))
+
+            return p, f, a, b
+        elif self._dimension == 1:
+
+            return d[0]*f, a, b
